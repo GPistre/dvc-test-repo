103d4eb8d5ed832bedae1eceda89c0e89f65cf91
Gérôme Pistre <gerome.pistre@density.io>
2023-01-05 09:39:32 +0100

    dev

--- dvc_debug.py
+++ dvc_debug.py
@@ -4,6 +4,6 @@ from dvc.api import DVCFileSystem
 
 if __name__ == '__main__':
     tag = '0.0.1'
-    fs = DVCFileSystem('.', rev=tag)
-    fs.ls('./data_from_repo')
+    fs = DVCFileSystem('.', rev='trunk')
+    print(fs.ls('./data_from_repo'))
     # print(fs.find("/", detail=False, dvc_only=True))
