4345b09df089e4e70f1bd07e05a6752cb67b9b0d
Gérôme Pistre <gerome.pistre@density.io>
2023-01-05 11:47:36 +0100

    explicite remote

--- dvc_debug.py
+++ dvc_debug.py
@@ -3,11 +3,14 @@ from dvc.api import DVCFileSystem
 from dvc.repo import Repo
 
 if __name__ == '__main__':
-    tag = '0.0.4'
+    tag = '0.0.5'
     repo = Repo('/Users/gpistre/Density/dvc-test-repo/',
                 rev=tag
                 )
     fs = DVCFileSystem(repo=repo)
-    print(fs.ls('./images'))
-    # print(fs.ls('./data_from_registry'))
+    # fs.open('images/test/0/00004.png')
+    # print(fs.ls('./images/test/0'))
+    # fs.open('./data_from_repo/titanic_train.csv')
+    # fs.open('./data_from_registry/titanic_train.csv')
+    print(fs.ls('./data_from_registry'))
     # print(fs.find("/", detail=False, dvc_only=True))
