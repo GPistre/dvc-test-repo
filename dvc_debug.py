eb4bab7ed500af3675011677bd9af1c383296c10
Gérôme Pistre <gerome.pistre@density.io>
2023-01-05 11:30:04 +0100

    images

--- dvc_debug.py
+++ dvc_debug.py
@@ -3,9 +3,11 @@ from dvc.api import DVCFileSystem
 from dvc.repo import Repo
 
 if __name__ == '__main__':
-    tag = '0.0.3'
-    repo = Repo('.', rev=tag)
+    tag = '0.0.4'
+    repo = Repo('/Users/gpistre/Density/dvc-test-repo/',
+                rev=tag
+                )
     fs = DVCFileSystem(repo=repo)
-    # print(fs.ls('./data_from_repo'))
-    print(fs.ls('./data_from_registry'))
+    print(fs.ls('./images'))
+    # print(fs.ls('./data_from_registry'))
     # print(fs.find("/", detail=False, dvc_only=True))
